fd0f2d569d9af8d867fe3d9ac618e9b6ab6b5802
Jorgen Lundman <lundman@lundman.net>
2020-05-06 10:43:46 +0900

    Upstream: additional changes to macos properties

--- module/zcommon/zfs_prop.c
+++ module/zcommon/zfs_prop.c
@@ -354,7 +354,7 @@ zfs_prop_init(void)
 	zprop_register_index(ZFS_PROP_SNAPDEV, "snapdev", ZFS_SNAPDEV_HIDDEN,
 	    PROP_INHERIT, ZFS_TYPE_FILESYSTEM | ZFS_TYPE_VOLUME,
 	    "hidden | visible", "SNAPDEV", snapdev_table);
-#ifdef __FreeBSD__
+#if defined (__FreeBSD__) || defined (__APPLE__)
 	zprop_register_index(ZFS_PROP_ACLMODE, "aclmode", ZFS_ACL_DISCARD,
 	    PROP_INHERIT, ZFS_TYPE_FILESYSTEM,
 	    "discard | groupmask | passthrough | restricted", "ACLMODE",
@@ -523,8 +523,8 @@ zfs_prop_init(void)
 	    "COM.APPLE.IGNOREOWNER", boolean_table);
 	zprop_register_hidden(ZFS_PROP_LASTUNMOUNT, "COM.APPLE.LASTUNMOUNT",
 	    PROP_TYPE_NUMBER, PROP_READONLY, ZFS_TYPE_DATASET, "LASTUNMOUNT");
-	zprop_register_index(ZFS_PROP_MIMIC, "com.apple.mimic_hfs", 0,
-	    PROP_INHERIT, ZFS_TYPE_FILESYSTEM, "on | off",
+	zprop_register_index(ZFS_PROP_MIMIC, "com.apple.mimic", 0,
+	    PROP_INHERIT, ZFS_TYPE_FILESYSTEM, "off | hfs | apfs",
 	    "COM.APPLE.MIMIC_HFS", mimic_table);
 	zprop_register_index(ZFS_PROP_DEVDISK, "com.apple.devdisk", 0,
 	    PROP_INHERIT, ZFS_TYPE_FILESYSTEM, "poolonly | on | off",
@@ -649,7 +649,7 @@ zfs_prop_init(void)
 	 * that we don't have to change the values of the zfs_prop_t enum, or
 	 * have NULL pointers in the zfs_prop_table[].
 	 */
-#ifdef __FreeBSD__
+#if defined (__FreeBSD__) || defined (__APPLE__)
 	zprop_register_impl(ZFS_PROP_ACLTYPE, "acltype", PROP_TYPE_INDEX,
 	    ZFS_ACLTYPE_OFF, NULL, PROP_INHERIT,
 	    ZFS_TYPE_FILESYSTEM | ZFS_TYPE_SNAPSHOT,
