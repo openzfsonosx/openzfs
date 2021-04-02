3d2305a5f253ec0666f4e00ee661de081f0d777c
Jorgen Lundman <lundman@lundman.net>
2021-04-01 23:03:49 -0700

    Upstream: Add zpool_disable_datasets_os()
    macOS needs to do a bunch of work to kick everything off zvols.

--- lib/libzfs/libzfs_mount.c
+++ lib/libzfs/libzfs_mount.c
@@ -1642,6 +1642,11 @@ zpool_disable_datasets(zpool_handle_t *zhp, boolean_t force)
 			remove_mountpoint(sets[i].dataset);
 	}
 
+#ifdef __APPLE__
+	/* We have to do a bunch of work to kick ZVOLs off */
+	zpool_disable_datasets_os(zhp, force);
+#endif
+
 	ret = 0;
 out:
 	(void) fclose(mnttab);
