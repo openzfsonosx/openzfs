38a624698c8e038d4715a7e3705a33080694b8e0
rottegift <smd+github@mis.use.net>
2021-04-09 11:30:52 +0100

    Make arc sysctl tunables work (#27)
    * use an IOMemAligned for a PAGE_SIZE allocation
    
    * we should call arc_kstat_update_osx()
    
    Changing kstat.zfs.darwin.tunable.zfs_arc_min doesn't do
    anything becasue arc_kstat_update_osx() was removed at the
    same time the (obsoleted by upstream) arc_kstat_update()
    was removed from zfs_kstat_osx.c.   Put it back.
    
    * when we sysctl arc tunables, call arc_tuning_update()
    
    * rely on upstream's sanity checking
    
    Simplification which also avoids spurious CMN_WARN
    messages caused by setting the arcstat variables here,
    when upstream's arc_tuning_update() checks that they
    differ from the tunable variables.
    
    * add tunable zfs_arc_sys_free and fix zfs_arc_lotsfree_percent
    
    both are in upstream's arc_tuning_update()
    
    zfs_arc_sys_free controls the amount of memory that ARC
    will leave free, which is roughly what lundman wants for
    putting some sort of cap on memory use.
    
    * cstyle

--- module/os/macos/zfs/arc_os.c
+++ module/os/macos/zfs/arc_os.c
@@ -698,89 +698,73 @@ int
 arc_kstat_update_osx(kstat_t *ksp, int rw)
 {
 	osx_kstat_t *ks = ksp->ks_data;
+	boolean_t do_update = B_FALSE;
 
 	if (rw == KSTAT_WRITE) {
 
 		/* Did we change the value ? */
 		if (ks->arc_zfs_arc_max.value.ui64 != zfs_arc_max) {
-
 			/* Assign new value */
 			zfs_arc_max = ks->arc_zfs_arc_max.value.ui64;
-
-			/* Update ARC with new value */
-			if (zfs_arc_max > 64<<20 && zfs_arc_max <
-			    physmem * PAGESIZE)
-				arc_c_max = zfs_arc_max;
-
-			arc_c = arc_c_max;
-			arc_p = (arc_c >> 1);
-
-			/* If meta_limit is not set, adjust it automatically */
-			if (!zfs_arc_meta_limit)
-				arc_meta_limit = arc_c_max / 4;
+			do_update = B_TRUE;
 		}
 
 		if (ks->arc_zfs_arc_min.value.ui64 != zfs_arc_min) {
 			zfs_arc_min = ks->arc_zfs_arc_min.value.ui64;
-			if (zfs_arc_min > 64<<20 && zfs_arc_min <= arc_c_max) {
-				arc_c_min = zfs_arc_min;
-				printf("ZFS: set arc_c_min %llu, arc_meta_min "
-				    "%llu, zfs_arc_meta_min %llu\n",
-				    arc_c_min, arc_meta_min, zfs_arc_meta_min);
-				if (arc_c < arc_c_min) {
-					printf("ZFS: raise arc_c %llu to "
-					    "arc_c_min %llu\n", arc_c,
-					    arc_c_min);
-					arc_c = arc_c_min;
-					if (arc_p < (arc_c >> 1)) {
-						printf("ZFS: raise arc_p %llu "
-						    "to %llu\n",
-						    arc_p, (arc_c >> 1));
-						arc_p = (arc_c >> 1);
-					}
-				}
-			}
+			do_update = B_TRUE;
 		}
 
 		if (ks->arc_zfs_arc_meta_limit.value.ui64 !=
 		    zfs_arc_meta_limit) {
 			zfs_arc_meta_limit =
 			    ks->arc_zfs_arc_meta_limit.value.ui64;
-
-			/* Allow the tunable to override if it is reasonable */
-			if (zfs_arc_meta_limit > 0 &&
-			    zfs_arc_meta_limit <= arc_c_max)
-				arc_meta_limit = zfs_arc_meta_limit;
-
-			if (arc_c_min < arc_meta_limit / 2 &&
-			    zfs_arc_min == 0)
-				arc_c_min = arc_meta_limit / 2;
-
-			printf("ZFS: set arc_meta_limit %llu, arc_c_min %llu,"
-			    "zfs_arc_meta_limit %lu\n",
-			    arc_meta_limit, arc_c_min, zfs_arc_meta_limit);
+			do_update = B_TRUE;
 		}
 
 		if (ks->arc_zfs_arc_meta_min.value.ui64 != zfs_arc_meta_min) {
 			zfs_arc_meta_min  = ks->arc_zfs_arc_meta_min.value.ui64;
-			if (zfs_arc_meta_min >= arc_c_min) {
-				printf("ZFS: probable error, zfs_arc_meta_min "
-				    "%llu >= arc_c_min %llu\n",
-				    zfs_arc_meta_min, arc_c_min);
-			}
-			if (zfs_arc_meta_min > 0 &&
-			    zfs_arc_meta_min <= arc_meta_limit)
-				arc_meta_min = zfs_arc_meta_min;
-			printf("ZFS: set arc_meta_min %llu\n", arc_meta_min);
+			do_update = B_TRUE;
 		}
 
-		zfs_arc_grow_retry = ks->arc_zfs_arc_grow_retry.value.ui64;
-		arc_grow_retry = zfs_arc_grow_retry;
-		zfs_arc_shrink_shift = ks->arc_zfs_arc_shrink_shift.value.ui64;
-		zfs_arc_p_min_shift = ks->arc_zfs_arc_p_min_shift.value.ui64;
-		zfs_arc_average_blocksize =
-		    ks->arc_zfs_arc_average_blocksize.value.ui64;
+		if (zfs_arc_grow_retry !=
+		    ks->arc_zfs_arc_grow_retry.value.ui64) {
+			zfs_arc_grow_retry =
+			    ks->arc_zfs_arc_grow_retry.value.ui64;
+			do_update = B_TRUE;
+		}
+		if (zfs_arc_shrink_shift !=
+		    ks->arc_zfs_arc_shrink_shift.value.ui64) {
+			zfs_arc_shrink_shift =
+			    ks->arc_zfs_arc_shrink_shift.value.ui64;
+			do_update = B_TRUE;
+		}
+		if (zfs_arc_p_min_shift !=
+		    ks->arc_zfs_arc_p_min_shift.value.ui64) {
+			zfs_arc_p_min_shift =
+			    ks->arc_zfs_arc_p_min_shift.value.ui64;
+			do_update = B_TRUE;
+		}
+		if (zfs_arc_average_blocksize !=
+		    ks->arc_zfs_arc_average_blocksize.value.ui64) {
+			zfs_arc_average_blocksize =
+			    ks->arc_zfs_arc_average_blocksize.value.ui64;
+			do_update = B_TRUE;
+		}
+		if (zfs_arc_lotsfree_percent !=
+		    ks->zfs_arc_lotsfree_percent.value.i64) {
+			zfs_arc_lotsfree_percent =
+			    ks->zfs_arc_lotsfree_percent.value.i64;
+			do_update = B_TRUE;
+		}
+		if (zfs_arc_sys_free !=
+		    ks->zfs_arc_sys_free.value.i64) {
+			zfs_arc_sys_free =
+			    ks->zfs_arc_sys_free.value.i64;
+			do_update = B_TRUE;
+		}
 
+		if (do_update)
+			arc_tuning_update(B_TRUE);
 	} else {
 
 		ks->arc_zfs_arc_max.value.ui64 = zfs_arc_max;
@@ -795,6 +779,10 @@ arc_kstat_update_osx(kstat_t *ksp, int rw)
 		ks->arc_zfs_arc_p_min_shift.value.ui64 = zfs_arc_p_min_shift;
 		ks->arc_zfs_arc_average_blocksize.value.ui64 =
 		    zfs_arc_average_blocksize;
+		ks->zfs_arc_lotsfree_percent.value.i64 =
+		    zfs_arc_lotsfree_percent;
+		ks->zfs_arc_sys_free.value.i64 =
+		    zfs_arc_sys_free;
 	}
 	return (0);
 }
