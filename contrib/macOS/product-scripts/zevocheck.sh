30077dfc9f69dabbb21795e2b1da453c4d828adc
Jorgen Lundman <lundman@lundman.net>
2021-06-08 19:41:56 +0900

    macOS: shellcheck zevocheck.sh

--- contrib/macOS/product-scripts/zevocheck.sh
+++ contrib/macOS/product-scripts/zevocheck.sh
@@ -3,9 +3,9 @@
 #exit code 1 means ZEVO is neither installed nor just uninstalled without reboot
 
 echo "ZEVO files check"
-&>/dev/null ls /System/Library/Extensions/ZFSDriver.kext/ && exit 0
-&>/dev/null ls /System/Library/Extensions/ZFSFilesystem.kext/ && exit 0
-&>/dev/null ls /Library/LaunchDaemons/com.getgreenbytes* && exit 0
+ls /System/Library/Extensions/ZFSDriver.kext/ &>/dev/null && exit 0
+ls /System/Library/Extensions/ZFSFilesystem.kext/ &>/dev/null && exit 0
+ls /Library/LaunchDaemons/com.getgreenbytes* &>/dev/null && exit 0
 
 echo "ZEVO launchctl check"
 /bin/launchctl list | grep greenbytes &>/dev/null
