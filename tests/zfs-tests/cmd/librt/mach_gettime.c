614717e4e5c81e31478029196e4c1785ceee3459
Jorgen Lundman <lundman@lundman.net>
2022-02-08 09:22:41 +0900

    macOS: fix up clock_gettime for zfs-tester

--- tests/zfs-tests/cmd/librt/mach_gettime.c
+++ tests/zfs-tests/cmd/librt/mach_gettime.c
@@ -1,31 +1,19 @@
+/*
+ * This relies on lib/libspl/include/os/macos/sys/time.h
+ * being included.
+ * Old macOS did not have clock_gettime, and current
+ * has it in libc. Linux assumes that we are to use
+ * librt for it, so we create this dummy library.
+ * The linker will sort out connecting it for us.
+ */
 #include <sys/types.h>
 #include <sys/_types/_timespec.h>
 #include <mach/mach.h>
 #include <mach/clock.h>
 #include <mach/mach_time.h>
 
-/* As used by test-runner.py */
-#define	CLOCK_MONOTONIC_RAW 4
-
-typedef int clockid_t;
-
-extern int clock_gettime(clockid_t clk_id, struct timespec *tp);
-
-int
-clock_gettime(clockid_t clk_id, struct timespec *tp)
+void
+gettime_dummy(void)
 {
-	int retval = 0;
-	if (clk_id == CLOCK_MONOTONIC_RAW) {
-		clock_serv_t cclock;
-		mach_timespec_t mts;
-
-		host_get_clock_service(mach_host_self(), CALENDAR_CLOCK,
-		    &cclock);
-		retval = clock_get_time(cclock, &mts);
-		mach_port_deallocate(mach_task_self(), cclock);
-
-		tp->tv_sec = mts.tv_sec;
-		tp->tv_nsec = mts.tv_nsec;
-	}
-	return (0);
+	clock_gettime(0, NULL);
 }
